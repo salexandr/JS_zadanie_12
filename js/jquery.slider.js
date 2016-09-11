85e146a83cf4973e6cb5bd508060afd6ee6be5d2
Alexandr <a.senia@yandex.ru>
2016-09-11 16:31:42 +0400

    gotovoe

---
+++ js/jquery.slider.js
@@ -0,0 +1,43 @@
+﻿
+(function($) { 
+
+$.fn.slider = function(options) {
+	
+	var defaults = {
+		over: 'green'
+	};
+	
+	var settings = $.extend(defaults, options);
+	
+	$('.carousel-element p').css({
+		'color': settings.over
+	});
+	
+    var leftUIEl = $('.carousel-arrow-left');
+    var rightUIEl = $('.carousel-arrow-right');
+    var elementsList = $('.carousel-list');
+ 
+    var pixelsOffset = 134;
+    var currentLeftValue = 0;
+    var elementsCount = elementsList.find('li').length;
+    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
+    var maximumOffset = 0;
+ 
+    leftUIEl.click(function() {        
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += 134;
+            elementsList.animate({ left : currentLeftValue + "px"}, 400);
+        }        
+    });
+ 
+    rightUIEl.click(function() {        
+        if (currentLeftValue != minimumOffset) {
+            currentLeftValue -= 134;
+            elementsList.animate({ left : currentLeftValue + "px"}, 400);
+        }        
+    });
+	
+	return this;
+};
+
+})(jQuery);
---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿$(function(){
+	
+	$('.carousel-list').slider({
+		over: 'black'
+	});
+	
+	var html = $('#shablon').html();
+	var info = {
+		titleH1: 'Сенча Александр Владимирович',
+		img: 'img/my-photo.jpg',
+		titleH3: 'Студент курса Go for IT',
+		titleUl: 'Хочу учить фронтенд, потому что:',
+		points: 'arr',
+		tel: '+380954041267',
+		fb: 'https://www.facebook.com/profile.php?id=100012484811911',
+		feedback: 'Сделаю все, чтобы стать профессиональным frontend разработчиком.'
+	}
+	
+	arr = [		    
+    	{pointLi: 'Мне нравится создавать сайты'},
+		{pointLi: 'Это творческое ремесло'},
+	    {pointLi: 'В IT сфере много инноваций'}
+	]
+	
+	var content = tmpl(html, info);
+	
+	$('body').append(content);
+	
+});
